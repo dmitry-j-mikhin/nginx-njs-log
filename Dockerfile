f03c0644d9fdae84abb205dadb103591524d7d6c
Dmitry Mikhin <dmikhin@webmonitorx.ru>
2023-08-19 11:37:02 +0300

    use 4.6.13 meganode

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ FROM nginx:1.24.0
 
 LABEL maintainer="Dmitry Mikhin <dmikhin@webmonitorx.ru>"
 
-RUN --mount=type=bind,target=/tmp,source=scripts,ro \
-    /tmp/build.sh
+RUN --mount=type=bind,target=/tmp/build,source=scripts,ro \
+    /tmp/build/build.sh
 
 ENTRYPOINT ["/docker-entrypoint.sh"]
 
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,56 +1,14 @@
 set -ex
 
-if ! getent group wallarm >/dev/null; then
-  groupadd --system \
-   wallarm 2>&1 >/dev/null
-fi
-if ! getent passwd wallarm >/dev/null; then
-  useradd --system --gid wallarm \
-   --home /opt/wallarm \
-   --comment "Wallarm system user" \
-   wallarm 2>&1 >/dev/null
-fi
-
-curl https://meganode.webmonitorx.ru/4.6/wallarm-4.6.12.x86_64-glibc.sh -O
-sh ./wallarm-4.6.12.x86_64-glibc.sh --noexec --target /opt/wallarm
-chmod a+x /opt/wallarm
-rm wallarm-4.6.12.x86_64-glibc.sh
+curl https://meganode.webmonitorx.ru/4.6/wallarm-4.6.13.x86_64-glibc.sh -O
+sh ./wallarm-4.6.13.x86_64-glibc.sh -- -b --skip-registration --skip-systemd
+rm wallarm-4.6.13.x86_64-glibc.sh
 find /opt/wallarm/modules \
     ! -wholename '/opt/wallarm/modules/stable-1240/*' \
     \( -type f -o -type l \) -delete
 find /opt/wallarm/modules -empty -type d -delete
-rm /opt/wallarm/setup.sh
-chown -R wallarm:wallarm /opt/wallarm
-
-cat << 'EOF' | tee "/etc/nginx/wallarm-status.conf" >/dev/null
-# wallarm-status, required for monitoring purposes.
-
-# Default `wallarm-status` configuration.
-# It is strongly advised not to alter any of the existing lines of the default
-# wallarm-status configuration as it may corrupt the process of metric data
-# upload to the Wallarm cloud.
-
-server {
-  listen 127.0.0.8:80;
-
-  server_name localhost;
-
-  allow 127.0.0.0/8;
-  deny all;
-
-  wallarm_mode off;
-  disable_acl "on";
-  access_log off;
-
-  location ~/wallarm-status$ {
-    wallarm_status on;
-  }
-}
-EOF
 
 sed -i \
- -e '/^events {/i load_module /opt/wallarm/modules/stable-1240/ngx_http_wallarm_module.so;' \
- -e '/^http {/a include /etc/nginx/wallarm-status.conf;' \
  -e '/^events {/i load_module modules/ngx_http_js_module.so;' \
  -e 's|access_log.*|access_log off;|' \
  /etc/nginx/nginx.conf
@@ -58,7 +16,7 @@ sed -i \
 sed -i \
  -e 's|listen.*|listen 8080;|g' \
  /etc/nginx/conf.d/default.conf
-cat /tmp/default_addon.conf >> /etc/nginx/conf.d/default.conf
+cat /tmp/build/default_addon.conf >> /etc/nginx/conf.d/default.conf
 
 sed -i \
  -e '/exec "$@"/i \
@@ -74,10 +32,10 @@ sed -i \
  if [ -n "$TARANTOOL_MEMORY_GB" ]; then\
   runuser -g wallarm -u wallarm -- sed -i -e "s|SLAB_ALLOC_ARENA=0.2|SLAB_ALLOC_ARENA=$TARANTOOL_MEMORY_GB|g" /opt/wallarm/env.list\
  fi\
- runuser -g wallarm -u wallarm -- /opt/wallarm/register-node $args\
- chroot --userspec=wallarm:wallarm / /opt/wallarm/supervisord.sh &\
+ /opt/wallarm/register-node $args\
+ /opt/wallarm/supervisord.sh &\
  /wait-for-it.sh -t 60 127.0.0.1:3313' \
  docker-entrypoint.sh
 
-cp /tmp/wait-for-it.sh /
-cp /tmp/logging.js /etc/nginx/
+cp /tmp/build/wait-for-it.sh /
+cp /tmp/build/logging.js /etc/nginx/
